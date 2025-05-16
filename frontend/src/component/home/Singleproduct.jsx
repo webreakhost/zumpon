18f12bf0f1d72184ff110e4dc7e9c0b42c63e232
iamsantoshku <santoshkr1592000@gmail.com>
2025-05-16 17:52:04 +0530

    singleprd

--- frontend/src/component/home/Singleproduct.jsx
+++ frontend/src/component/home/Singleproduct.jsx
@@ -71,7 +71,7 @@ const SingleProduct = () => {
 
   return (
     <div className="single-product p-8 bg-white rounded-xl shadow-lg max-w-12xl mx-auto h-auto md:h-auto">
-      <h4 className="text-2xl font-bold text-center mb-6 text-gray-800 border-b pb-2">
+      <h4 className="text-2xl font-bold text-left mb-6 text-gray-800 border-b pb-2">
         Deals Hot of The Day
       </h4>
 
@@ -80,7 +80,7 @@ const SingleProduct = () => {
           <div className="main-content1">
             <div className="product product-single row">
               <div className="col-md-6 mb-6">
-                <div className="product-gallery product-gallery-sticky">
+                {/* <div className="product-gallery product-gallery-sticky">
                   <div className="swiper-container product-single-swiper swiper-theme nav-inner">
                     <div className="swiper-wrapper row cols-1 gutter-no">
                       {selectedImage ? (
@@ -125,13 +125,82 @@ const SingleProduct = () => {
                       )}
                     </div>
                   </div>
+                </div> */}
+
+                <div className="product-gallery product-gallery-sticky product-gallery-vertical">
+                  <div className="swiper-container product-single-swiper swiper-theme nav-inner">
+                    <div className="swiper-wrapper row cols-1 gutter-no">
+                      {selectedImage ? (
+                        <div className="swiper-slide">
+                          <figure className="product-image img2">
+                            <img
+                              src={`${BACKENDURL}/uploads/product/${selectedImage.split("\\").pop()}`}
+                              alt={product.name}
+                              width="800"
+                              height="900"
+                            />
+                          </figure>
+                        </div>
+                      ) : (
+                        <p>No images available</p>
+                      )}
+                    </div>
+                    {/* <button className="swiper-button-next"></button>
+                    <button className="swiper-button-prev"></button> */}
+                    <div className="product-label-group ">
+                      <label className="product-label label-discount ml-10">25% Off</label>
+                    </div>
+                  </div>
+
+                  <div
+                    className="product-thumbs-wrap swiper-container"
+                    data-swiper-options={JSON.stringify({
+                      direction: "vertical",
+                      breakpoints: {
+                        0: {
+                          direction: "horizontal",
+                          slidesPerView: 4,
+                        },
+                        992: {
+                          direction: "vertical",
+                          slidesPerView: "auto",
+                        },
+                      },
+                    })}
+                  >
+                    <div className="product-thumbs swiper-wrapper row cols-lg-1 cols-4 gutter-sm">
+                      {product.images && product.images.length > 0 ? (
+                        product.images.map((imageObj, index) =>
+                          imageObj.urls.map((url, i) => (
+                            <div
+                              className="product-thumb swiper-slide"
+                              key={`${index}-${i}`}
+                              onClick={() => setSelectedImage(url)}
+                              style={{ cursor: "pointer" }}
+                            >
+                              <img
+                                src={`${BACKENDURL}/uploads/product/${url.split("\\").pop()}`}
+                                alt={`Product Thumbnail ${index + 1}`}
+                                width="60"
+                                height="68"
+                              />
+                            </div>
+                          ))
+                        )
+                      ) : (
+                        <p>No thumbnail images</p>
+                      )}
+                    </div>
+                  </div>
                 </div>
+
               </div>
 
-              <div className="col-md-6 mb-4 mb-md-6">
+              <div className="col-md-6 mb-2 mb-md-6">
                 <div className="product-details">
                   <h1 className="product-title">{product.name || "No Name"}</h1>
-                  
+                  <hr className="product-divider" />
+
 
                   <div className="product-price">
                     <ins className="new-price">${product.price || "0.00"}</ins>
@@ -141,7 +210,7 @@ const SingleProduct = () => {
                     <h3 className="">description:{product.description || "N/A"}</h3>
                   </div>
 
-                  <div className="product-short-desc">
+                  {/* <div className="product-short-desc">
                     <ul className="list-type-check list-style-none">
                       {product.features && product.features.length > 0 ? (
                         product.features.map((feature, index) => <li key={index}>{feature}</li>)
@@ -149,11 +218,24 @@ const SingleProduct = () => {
                         <li>No features listed</li>
                       )}
                     </ul>
+                  </div> */}
+
+                  <div class="product-countdown-container flex-wrap mt-3">
+                    <label className="mr-2 text-default">Offer Ends In:</label>
+                    <div
+                      className="product-countdown countdown-compact"
+                      data-until="2022, 12, 31"
+                      data-compact="true"
+                      style={{ fontWeight: "bold", color: "rgb(60, 120, 180)" }}
+                    >
+                      629 days, 11: 59: 52
+                    </div>
+
                   </div>
 
-                  <hr className="product-divider" />
+                  {/* <hr className="product-divider" /> */}
 
-                  <div className="product-form product-variation-form product-color-swatch">
+                  {/* <div className="product-form product-variation-form product-color-swatch">
                     <label>Color:</label>
                     <div className="d-flex align-items-center product-variations">
                       {product.colors && product.colors.length > 0 ? (
@@ -161,9 +243,8 @@ const SingleProduct = () => {
                           <a
                             href="#"
                             key={index}
-                            className={`color ${
-                              selectedColor === colorObj.color ? "selected" : ""
-                            }`}
+                            className={`color ${selectedColor === colorObj.color ? "selected" : ""
+                              }`}
                             style={{
                               backgroundColor: colorObj.color,
                               width: "30px",
@@ -182,6 +263,13 @@ const SingleProduct = () => {
                         <p>No colors available</p>
                       )}
                     </div>
+                  </div> */}
+                  <div className="ratings-container">
+                    <div className="ratings-full">
+                      <span className="ratings" style={{ width: "80%" }}></span>
+                      <span className="tooltiptext tooltip-top"></span>
+                    </div>
+                    <a href="#" className="rating-reviews">(3 Reviews)</a>
                   </div>
 
                   <div className="product-form product-variation-form product-size-swatch">
@@ -192,9 +280,8 @@ const SingleProduct = () => {
                           <a
                             href="#"
                             key={sizeObj._id}
-                            className={`size ${
-                              selectedSize === sizeObj.size ? "selected" : ""
-                            }`}
+                            className={`size ${selectedSize === sizeObj.size ? "selected" : ""
+                              }`}
                             onClick={(e) => {
                               e.preventDefault();
                               setSelectedSize(sizeObj.size);
@@ -208,26 +295,71 @@ const SingleProduct = () => {
                       )}
                     </div>
                   </div>
-
+                  {/* <button className="btn btn-primary btn-cart" onClick={handleAddToCart}>
+                      <i className="w-icon-cart"></i>
+                      <span>Add to Cart</span>
+                    </button> */}
+                  {/* 
                   <div className="product-form container">
-                    {/* <div className="product-qty-form">
+                    
+                    
+                     <div className="product-qty-form mb-2 mr-2 border-black">
                       <div className="input-group">
-                        <input
-                          className="quantity form-control"
-                          type="number"
-                          min="1"
-                          max="10000000"
-                          defaultValue="1"
-                        />
+                        <input classn
+                          ="quantity form-control" type="number"
+                          min="1" max="10000000" />
                         <button className="quantity-plus w-icon-plus"></button>
-                        <button className="quantity-minus w-icon-minus"></button>
+                        <button
+                          className="quantity-minus w-icon-minus"></button>
                       </div>
-                    </div> */}
-                    <button className="btn btn-primary btn-cart" onClick={handleAddToCart}>
+                    </div>
+                    <button className="btn btn-primary btn-cart">
                       <i className="w-icon-cart"></i>
                       <span>Add to Cart</span>
                     </button>
+                  </div> */}
+
+                  {/* <div className=" container d-flex align-items-center justify-content-start mt-1">
+  <div className="product-qty-form mb-2 mr-3" style={{ border: "1px solid #d1d5db", padding: "4px", borderRadius: "5px" }}>
+    <div className="input-group d-flex align-items-center">
+      <button className="quantity-minus w-icon-minus" style={{ border: "none", background: "transparent", cursor: "pointer" }}></button>
+      <input
+        className="quantity form-control text-center"
+        type="number"
+        min="1"
+        max="10000000"
+        defaultValue="1"
+        style={{ width: "70px", border: "none", outline: "none" }}
+      />
+      <button className="quantity-plus w-icon-plus" style={{ border: "none", background: "transparent", cursor: "pointer" }}></button>
+    </div>
+  </div>
+
+  <button className="btn btn-primary btn-cart ml-3">
+    <i className="w-icon-cart"></i>
+    <span>Add to Cart</span>
+  </button>
+</div> */}
+
+                  <div class="product-form pt-2">
+                    <div class="product-qty-form mb-2 mr-2">
+                      <div class="input-group">
+                        <input class="quantity form-control" type="number"
+                          min="1" max="10000000" />
+                        <button class="quantity-plus w-icon-plus"></button>
+                        <button
+                          class="quantity-minus w-icon-minus"></button>
+                      </div>
+                    </div>
+                    <button class="btn btn-primary btn-cart">
+                      <i class="w-icon-cart"></i>
+                      <span>Add to Cart</span>
+                    </button>
                   </div>
+
+
+
+
 
                   <div className="social-links-wrapper">
                     <div className="social-links">
@@ -245,7 +377,7 @@ const SingleProduct = () => {
             </div>
           </div>
         </div>
-      </div> 
+      </div>
     </div>
   );
 };
